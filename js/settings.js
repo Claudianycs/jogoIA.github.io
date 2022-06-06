332a3053a876092dcf974d67d89cd3ed199619cf
Claudiany Sousa <65419184+Claudianycs@users.noreply.github.com>
2022-06-06 00:34:47 -0300

    Add files via upload

---
+++ js/settings.js
@@ -0,0 +1,17 @@
+let myVar = setInterval(myTimer, 1000);
+
+var count = 30;
+
+function myTimer() {
+  count--
+  document.getElementById("timer").innerText=count;
+
+  if(count == 0){
+      document.getElementById("").innerHTML
+  }else{
+
+  }
+}
+function myScore() {
+    
+  }
